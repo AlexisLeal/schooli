7b4f76b760028e7c312364366ba297cafea231ea
AlexisLeal <31640145+AlexisLeal@users.noreply.github.com>
2020-10-28 19:04:24 -0600

    ..
    ..

--- app/Views/evaluaciones/crear/agregar_preguntas.php
+++ app/Views/evaluaciones/crear/agregar_preguntas.php
@@ -1,5 +1,7 @@
 
 <?php include(APPPATH.'/Views/include/header.php');?>
+
+
 <style>
 
 </style>
@@ -171,112 +173,11 @@
 </div>
 
 <!--Esta linea sera temporal-->
-<?php include(APPPATH.'/Views/include/header-js.php');?>
+
     <script src="js/bootstrap.min.js"></script>
     <script type="text/javascript" src="DataTables/datatables.min.js"></script>
-    <script>
-  
-//var lista = document.getElementById("tipoPregunta");
-//var valorSeleccionado = lista.options[lista.selectedIndex].value;
-//var textoSeleccionado = lista.options[lista.selectedIndex].text;
-// Ya tenemos el valor seleccionado, ahora vamos a mostrar los input necesarios.
-
-// Validacion para habialitar un upload y cargar una imagen.
-$("#activarCargarAudioPregunta" ).click(function() {
-  // detectar si esta seleccionado o deseleccionado
-  let elemento = document.getElementById("activarCargarAudioPregunta");
-  // si esta cheked
-  if( elemento.checked ) {
-    $('#agregarAudioPregunta').append('<div class="form-group">\
-          Cargar imagen <input class="form-control" type="file" id="archivo_audio_pregunta" name="archivo_audio_pregunta" required="">\
-          </div>');    
-  }else{
-    document.getElementById("agregarAudioPregunta").innerHTML = "";
-  }
-});
 
 
-
-
-// Validacion para habialitar un upload y cargar una imagen.
-$("#activarCargarImagen" ).click(function() {
-  // detectar si esta seleccionado o deseleccionado
-  let elemento = document.getElementById("activarCargarImagen");
-  // si esta cheked
-  if( elemento.checked ) {
-    $('#agregarImagen').append('<div class="form-group">\
-          Cargar imagen <input class="form-control" type="file" id="archivo_imagen" name="archivo_imagen" required="">\
-          </div>');    
-  }else{
-    document.getElementById("agregarImagen").innerHTML = "";
-  }
-});
-
-
-
-
-// Comienza la logica para agregar los diferentes tipos de campos
-$("#tipoPregunta").on ('change', function(e) {
-var valor        = $(this).val();
-var idEvaluacion = 1;
-function removerDiv(){
-$agregarCampos.parent('div').remove();
-}
-switch(valor) {
-case "1":
-document.getElementById("agregarCampos").innerHTML = "";
-break;
-case "2":
-// Pregunta opcion multiple
-  var numCampos = 5;
-  var i=1;      
-  document.getElementById("agregarCampos").innerHTML = "";
-  if(document.querySelector('#agregarCampos').childElementCount < 1){ 
-  while (i < numCampos) {
-    var nombreCampo = idEvaluacion + '-' + valor + '-' + i;  
-    $('#agregarCampos').append('<div class="form-group mb-2">\
-          Opcion '+ i +' <input class="form-control" type="text" id="opcion_'+i+'" name="opcion_'+i+'" required="">\
-          </div>\
-          <div class="form-group mb-2">\
-          <input class="form-check-input" type="radio" name="opcion_correcta" id="'+i+'" value="'+i+'">\
-          <label class="form-check-label" for="exampleRadios1">\
-          Opción correcta <br/><br/><br/>\
-          </label>\
-          </div>\
-          ');
-      i++;
-    }
-  }
-break;
-case "3":
-// Audio
-document.getElementById("agregarCampos").innerHTML = "";
-if(document.querySelector('#agregarCampos').childElementCount < 1){ 
-var nombreCampo = idEvaluacion + '-' + valor;
-$('#agregarCampos').append('<div class="form-group">\
-          Cargar audio <input class="form-control" type="file" id="archivo_audio" name="archivo_audio" required="">\
-          </div>');
-}    
-break;
-case "4":
-// Video
-document.getElementById("agregarCampos").innerHTML = "";
-if(document.querySelector('#agregarCampos').childElementCount < 1){ 
-var nombreCampo = idEvaluacion + '-' + valor;
-$('#agregarCampos').append('<div class="form-group">\
-          Cargar video <input  class="form-control" type="file" id="archivo_video" name="archivo_video" required="">\
-          </div>');
-}
-break;
-case "5":
-// Falso o Verdadero
-document.getElementById("agregarCampos").innerHTML = "";
-break;
-}
-
-});
-
-</script>
-
-
+<?php include(APPPATH.'/Views/include/header-js.php');?>
+<script src="<?php echo base_url('js/Preguntas-Validacion.js'); ?>"></script>
 <?php include(APPPATH.'Views/include/footer.php');?>
---
+++ js/Preguntas-Validacion.js
@@ -0,0 +1,188 @@
+$(document).on('change','input[type="file"]',function(){
+	
+    
+    let audiocheck = document.getElementById("activarCargarAudioPregunta");
+    let imagen = document.getElementById("activarCargarImagen");
+    indice = document.getElementById("tipoPregunta").selectedIndex;
+
+	if( imagen.checked ) {
+     // this.files[0].size recupera el tamaño del archivo
+    // alert(this.files[0].size);
+	var fileName = this.files[0].name;
+	var fileSize = this.files[0].size;
+
+	if(fileSize > 3000000){
+		alert('El archivo no debe superar los 3MB');
+		this.value = '';
+		this.files[0].name = '';
+	}else{
+		// recuperamos la extensión del archivo
+		var ext = fileName.split('.').pop();
+		
+		// Convertimos en minúscula porque 
+		// la extensión del archivo puede estar en mayúscula
+		ext = ext.toLowerCase();
+    
+		// console.log(ext);
+		switch (ext) {
+			case 'jpg':
+			case 'jpeg':
+			case 'png':
+			case 'pdf': break;
+			default:
+				alert('El archivo no tiene la extensión adecuada');
+				this.value = ''; // reset del valor
+				this.files[0].name = '';
+		}
+	}
+}if( audiocheck.checked || indice == 3) {
+	var fileName = this.files[0].name;
+	var fileSize = this.files[0].size;
+
+	if(fileSize > 3000000){
+		alert('El archivo no debe superar los 3MB');
+		this.value = '';
+		this.files[0].name = '';
+	}else{
+		// recuperamos la extensión del archivo
+		var ext = fileName.split('.').pop();
+		
+		// Convertimos en minúscula porque 
+		// la extensión del archivo puede estar en mayúscula
+		ext = ext.toLowerCase();
+    
+		// console.log(ext);
+		switch (ext) {
+			case 'mp3':
+		 break;
+			default:
+				alert('El archivo no tiene la extensión adecuada');
+				this.value = ''; // reset del valor
+				this.files[0].name = '';
+		}
+	}
+//Video 
+}if(indice == 4){
+    var fileName = this.files[0].name;
+	var fileSize = this.files[0].size;
+
+	if(fileSize > 3000000){
+		alert('El archivo no debe superar los 3MB');
+		this.value = '';
+		this.files[0].name = '';
+	}else{
+		// recuperamos la extensión del archivo
+		var ext = fileName.split('.').pop();
+		
+		// Convertimos en minúscula porque 
+		// la extensión del archivo puede estar en mayúscula
+		ext = ext.toLowerCase();
+    
+		// console.log(ext);
+		switch (ext) {
+			case 'mp4':
+		 break;
+			default:
+				alert('El archivo no tiene la extensión adecuada');
+				this.value = ''; // reset del valor
+				this.files[0].name = '';
+		}
+	}
+
+}
+});
+
+// Validacion para habialitar un upload y cargar una imagen.
+$("#activarCargarAudioPregunta" ).click(function() {
+    // detectar si esta seleccionado o deseleccionado
+    let elemento = document.getElementById("activarCargarAudioPregunta");
+    // si esta cheked
+    if( elemento.checked ) {
+      $('#agregarAudioPregunta').append('<div class="form-group">\
+            Cargar imagen <input class="form-control" type="file" id="archivo_audio_pregunta" name="archivo_audio_pregunta" required="">\
+            </div>');    
+    }else{
+      document.getElementById("agregarAudioPregunta").innerHTML = "";
+    }
+  });
+  
+  
+  
+  
+  // Validacion para habialitar un upload y cargar una imagen.
+  $("#activarCargarImagen" ).click(function() {
+    // detectar si esta seleccionado o deseleccionado
+    let elemento = document.getElementById("activarCargarImagen");
+    // si esta cheked
+    if( elemento.checked ) {
+      $('#agregarImagen').append('<div class="form-group">\
+            Cargar imagen <input class="form-control" type="file" id="archivo_imagen" name="archivo_imagen" required="">\
+            </div>');    
+    }else{
+      document.getElementById("agregarImagen").innerHTML = "";
+    }
+  });
+  
+  
+  
+  
+  // Comienza la logica para agregar los diferentes tipos de campos
+  $("#tipoPregunta").on ('change', function(e) {
+  var valor        = $(this).val();
+  var idEvaluacion = 1;
+  function removerDiv(){
+  $agregarCampos.parent('div').remove();
+  }
+  switch(valor) {
+  case "1":
+  document.getElementById("agregarCampos").innerHTML = "";
+  break;
+  case "2":
+  // Pregunta opcion multiple
+    var numCampos = 5;
+    var i=1;      
+    document.getElementById("agregarCampos").innerHTML = "";
+    if(document.querySelector('#agregarCampos').childElementCount < 1){ 
+    while (i < numCampos) {
+      var nombreCampo = idEvaluacion + '-' + valor + '-' + i;  
+      $('#agregarCampos').append('<div class="form-group mb-2">\
+            Opcion '+ i +' <input class="form-control" type="text" id="opcion_'+i+'" name="opcion_'+i+'" required="">\
+            </div>\
+            <div class="form-group mb-2">\
+            <input class="form-check-input" type="radio" name="opcion_correcta" id="'+i+'" value="'+i+'">\
+            <label class="form-check-label" for="exampleRadios1">\
+            Opción correcta <br/><br/><br/>\
+            </label>\
+            </div>\
+            ');
+        i++;
+      }
+    }
+  break;
+  case "3":
+  // Audio
+  document.getElementById("agregarCampos").innerHTML = "";
+  if(document.querySelector('#agregarCampos').childElementCount < 1){ 
+  var nombreCampo = idEvaluacion + '-' + valor;
+  $('#agregarCampos').append('<div class="form-group">\
+            Cargar audio <input class="form-control" type="file" id="archivo_audio" name="archivo_audio" required="">\
+            </div>');
+  }    
+  break;
+  case "4":
+  // Video
+  document.getElementById("agregarCampos").innerHTML = "";
+  if(document.querySelector('#agregarCampos').childElementCount < 1){ 
+  var nombreCampo = idEvaluacion + '-' + valor;
+  $('#agregarCampos').append('<div class="form-group">\
+            Cargar video <input  class="form-control" type="file" id="archivo_video" name="archivo_video" required="">\
+            </div>');
+  }
+  break;
+  case "5":
+  // Falso o Verdadero
+  document.getElementById("agregarCampos").innerHTML = "";
+  break;
+  }
+  
+  });
