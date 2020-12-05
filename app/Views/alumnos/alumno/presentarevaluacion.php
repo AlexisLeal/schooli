d514f3ba5c50aa10ba2cea193c0d78bf4bf17b2a
iscgerardoflores <isc.gerardoflores@gmail.com>
2020-12-05 11:10:41 -0600

    gf cambios

--- app/Views/alumnos/alumno/presentarevaluacion.php
+++ app/Views/alumnos/alumno/presentarevaluacion.php
@@ -64,26 +64,256 @@
             <h4><?php echo $session->get('nombre')." ".$session->get('apellido')." ".$session->get('apellido_materno');?></h4>
           </div>
           <div class="espacioUno"></div>
-
+            <?php
+            //$data['idEvaluacion'] = $id_evaluacion;
+            //$data['nombre'] = $row->nombre;
+            //$nombre =getTipoEvaluacionEspecifico($row->tipo_evaluacion);
+            //$data['tipo_evaluacion'] = $nombre->nombre;
+            //$data['clave'] = $row->clave;
+            //$data['valorpreguntas'] =  getValorTotalPreguntas($id_evaluacion);
+            
+            /*
+            $data['idtipoevaluacion'] = $row->tipo_evaluacion;
+            $data['nivel'] = $row->nivel;	
+            $data['leccion'] = $row->leccion;
+            $data['totalpreguntas'] = getTotalPreguntas($id_evaluacion);
+            */
+            
+            ?>
           
+            <div class="card">
+              <div class="card-body">
+              Evaluación.<br/>
+              <hr class="linea"/>
+              <div style="padding-left:20px">
               <div class="card">
-                <div class="card-body">
-                Evaluación.<br/>
-                <hr class="linea"/>
-                <div style="padding-left:20px">
-                <div class="card">
-                <div class="card-body">
+              <div class="card-body">
+              
+              <table width="80%">
+              <tr><td><p class="font-weight-bold">Nombre de evaluacion:</p></td><td><?php echo $nombre;?></td></tr>
+              <tr><td><p class="font-weight-bold">Tipo Evaluacion:</p></td><td><?php echo $tipo_evaluacion;?></td></tr>
+              <tr><td><p class="font-weight-bold">Clave de evaluación:</p></td><td><?php echo $clave;?></td></tr>
+              <tr><td><p class="font-weight-bold">Valor total.</p></td><td><?php echo $valorpreguntas;?></td></tr>
+              </table>
+
+
+              <div class="row">
+                <div class="col-md-2 text-center">
+                <p class="font-weight-bold">No.</p>
+                </div>
+                <div class="col-md-8">
+                <p class="font-weight-bold">Pregunta</p>
+                </div>
+                <div class="col-md-2 text-center">
+                <p class="font-weight-bold">Valor</p>
+                </div>        
+              </div>
+
+
+              <?php                                
+              foreach(getPreguntas($idEvaluacion) as $fila){
+              ?>
+              <div class="row">
+                  <div class="col-md-2 text-center">
+                  <?php echo $fila->num_pregunta;?>
+                  </div>
+                  <div class="col-md-8">
+                  <?php
+                  echo $fila->pregunta;
+
+                  if($fila->tiene_imagen==1 || $fila->tiene_audio_pregunta==1){
+                      if($fila->tiene_imagen==1){
+                        ?>
+                        <img src="<?php echo base_url($fila->ruta_imagen);?>" class = "img-fluid" alt="Imagen" ><br/>
+                        <?php
+                      }
+                      if($fila->tiene_audio_pregunta==1){
+                        ?>
+                        <audio class="asado img-fliud" name="" id ="" src="<?php echo base_url($fila->ruta_audio_pregunta);?>" controls></audio><br/>
+                        
+
+                        <?php
+                      }
+                  }else{
+                      
+                  }
+                  ?>
+                  </div>
+                  <div class="col-md-2 text-center">
+                  <?php echo $fila->valor;?>
+                  </div>
+              </div>
+
+
+
+
+
+              <?php
+          $idPregunta = $fila->num_pregunta;
+          switch ($fila->idTipoPregunta){
+          case 1: // Pregunta abierta
+            ?>
+            <div class="row">
+              <div class="col-md-2 text-center">
+              
+              </div>
+              <div class="col-md-8">
+              <input type="text" class="form-control" name="<?php echo "ID".$fila->id."_EVAL".$fila->idEvaluacion."_NP".$fila->num_pregunta.""?>"
+              id="<?php echo "ID".$fila->id."EVAL".$fila->idEvaluacion."NP".$fila->num_pregunta.""?>">
+              </div>
+              <div class="col-md-2 text-center">
+              
+              </div>
+            </div>
+          <?php
+              break;
+
+            case 2:
+              // si es opcion multiple
+              ?>
+              <div class="row">
+                <div class="col-md-2 text-center">
+                
+                </div>
+                <div class="col-md-8">
+                <?PHP
+                $pregunta_multiple = getPreguntaOpcion_multiple($idEvaluacion,$idPregunta);
+                ?>
+                <table>
+                  <tr>
+                  <td><?php echo empty($pregunta_multiple->valor1) ? 0 : $pregunta_multiple->valor1;?> <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2"></td>
+                  <td><?php echo empty($pregunta_multiple->valor2) ? 0 : $pregunta_multiple->valor2;?> <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2"></td>
+                  <td><?php echo empty($pregunta_multiple->valor3) ? 0 : $pregunta_multiple->valor3;?> <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2"></td>
+                  <td><?php echo empty($pregunta_multiple->valor4) ? 0 : $pregunta_multiple->valor4;?> <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2"></td>
+                </tr>
+              </table>
+                
+          </div>
+          <div class="col-md-2 text-center">
+          
+          </div>
+          </div>
+          <?php
+          break;
+          case 3://audio
+            ?>
+            <div class="row">
+              <div class="col-md-2 text-center">
+              </div>
+              <div class="col-md-8">
+              <?PHP
+              $pregunta_audio = getPreguntaOpcion_audio($idEvaluacion,$idPregunta);
+            ?>
+          <audio class="asado img-fluid" name="" id ="" src="<?php echo base_url($pregunta_audio->ruta_audio);?>" controls></audio>
+          <textarea class="form-control" name="<?php echo "ID".$fila->id."EVAL".$fila->idEvaluacion."NP".$fila->num_pregunta.";"?>" id="<?php echo "ID".$fila->id."EVAL".$fila->idEvaluacion."NP".$fila->num_pregunta.";"?>" rows="3"></textarea>
+            </div>
+            <div class="col-md-2 text-center">
+              
+            </div>
+            </div>
+          <?php
+              break;
+
+          case 4:// video
+            ?>
+            <div class="row">
+              <div class="col-md-2 text-center">
+              </div>
+              <div class="col-md-8">
+              <?PHP
+              $pregunta_video = getPreguntaOpcion_video($idEvaluacion,$idPregunta);
+              ?>
+              <video name="" id="" class = "img-fluid" src="<?php echo $pregunta_video == null ? "desconocido" :base_url($pregunta_video->ruta_video);?>" controls>
+                Tu navegador no implementa el elemento <code>video</code>.
+              </video>
+          <textarea class="form-control" name="<?php echo "ID".$fila->id."EVAL".$fila->idEvaluacion."NP".$fila->num_pregunta.";"?>" id="<?php echo "ID".$fila->id."EVAL".$fila->idEvaluacion."NP".$fila->num_pregunta.";"?>" rows="3"></textarea>
+
+          </div>
+              <div class="col-md-2 text-center">
+
+              </div>
+            </div>
+          <?php
+              break;
+
+          case 5:// falso o verdadero
+            ?>
+            <div class="row">
+              <div class="col-md-2 text-center">
+
+              </div>
+              <div class="col-md-8">
+              <div class="form-check">
+                  <input class="form-check-input" type="radio" name="<?php echo "ID".$fila->id."EVAL".$fila->idEvaluacion."NP".$fila->num_pregunta.";"?>" id="radio1" value="verdadero">
+                  <label class="form-check-label" for="exampleRadios1">
+                  Verdadero
+                  </label>
+                  </div>
+                  <div class="form-check">
+                  <input class="form-check-input" type="radio" name="<?php echo "ID".$fila->id."EVAL".$fila->idEvaluacion."NP".$fila->num_pregunta.";"?>" id="radio2" value="falso">
+                  <label class="form-check-label" for="exampleRadios2">
+                  Falso
+                  </label>
+                  </div>
+
+
+              </div>
+              <div class="col-md-2 text-center">
+
+              </div>
+            </div>
+          <?php
+              break;
+            }
+            ?>
+            <div class="espacioUno"></div>
+            <hr class="lineaSolida">
+            <div class="espacioUno"></div>
+            
+            <?php
+            }
+            ?>
+
+
+        <div class="container">
+        <div class="row">
+            
+            <div class="col-md-2 text-center">
+              </div>
+            
+              <div class="col-md-8 text-center">
+                <button class="btn btn-primary" type="submit" name="" id="">Guardar</button>
+              </div>
+          
+              <div class="col-md-2 text-center">
+              </div>
+          
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
 
 
               </div>
             </div>
-            <br/>
-
+            
+            <!--
             <div class="card">
-                  <div class="card-body">
+                <div class="card-body">
 
-                  </div>
                 </div>
+            </div>
+            -->
 
 
 
