c7756912f81d1e5323cee08491a37c8ef0316e0b
AlexisLeal <31640145+AlexisLeal@users.noreply.github.com>
2020-12-18 15:43:37 -0600

    valor asistencia
    Se agrega el contrlador que define el valor que llevara la asitencia en una mencioanda frecuencia

---
+++ app/Models/Valor_asistencia_model.php
@@ -0,0 +1,27 @@
+<?php namespace App\Models;
+
+use CodeIgniter\Model;
+
+class Valor_asistencia_model extends Model
+{
+    #Nombre de la tabla
+    protected $table      = 'valor_asistencia';
+    #nombre de la clave primaria 
+    protected $primaryKey = 'id';
+
+    protected $returnType     = 'array';
+    protected $useSoftDeletes = true;
+
+    #Aqui ponemos el nombre de las columnas que vamos a modifcar
+    protected $allowedFields = ['valor','id_frecuencia','fecha_creacion','fecha_ultimo_cambio'];
+
+    protected $useTimestamps = false;
+    protected $createdField  = 'fecha_creacion';
+    protected $updatedField  = 'fecha_ultimo_cambio';
+    protected $deletedField  = 'deleted';
+
+    #Reglas de validacion 
+    protected $validationRules    = [];
+    protected $validationMessages = [];
+    protected $skipValidation     = false;
+}
