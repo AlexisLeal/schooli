75c207c05d6837ce673d3bbdd51290b5d150a6c9
iscgerardoflores <isc.gerardoflores@gmail.com>
2020-12-17 18:05:32 -0600

    gf agregar grupo

--- app/Views/grupos/crear/agregar_grupo.php
+++ app/Views/grupos/crear/agregar_grupo.php
@@ -38,170 +38,156 @@
 </header>
 
 
+<div class="container">
+  <div id="general">
+    <div class="row">
+      <div class="col-md-3">
+        <?php include(APPPATH.'/Views/include/menu-izquierda.php');?>
+      </div>
 
+      <div class="col-md-9">
+        <?php include(APPPATH.'/Views/include/notificacion.php');?>          
+        <?php if($session->has('Grupo')){;?>
+        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+          <strong>Notificaciones del sistema:</strong> <?php echo $session->get('Grupo')?>
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div> 
+        <?php } $session->remove('Grupo');?>
 
-
-    <!--Ejemplo tabla con DataTables-->
-    <div class="container">
-      <div id="general">
-        <div class="row">
-          <div class="col-md-3">
-            <?php include(APPPATH.'/Views/include/menu-izquierda.php');?>
-          </div>
-
-
-           
-          <div class="col-md-9">
-          <?php include(APPPATH.'/Views/include/notificacion.php');?>
-          
-          <?php if($session->has('Grupo')){;?>
-         <div class="alert alert-warning alert-dismissible fade show" role="alert">
-                <strong>Notificaciones del sistema:</strong> <?php echo $session->get('Grupo')?>
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div> 
-              <?php } $session->remove('Grupo');?>
-
-
-
-          <h4>Registro de un Grupo.</h4>
-          <div class="espacioUno"></div>
+      <h4>Registro de un Grupo.</h4>
+      <div class="espacioUno"></div>
         
 
-              <div class="card">
-                <div class="card-body">
-
-                <form action="<?php echo site_url('Grupos/insertargrupo');?>" method="post" enctype="multipart/form-data" id = "datosgrupo">
-               
-                <div class="espacioDos"></div>
-                <div class="row">
-                  <div class="col">
+        <div class="card">
+          <div class="card-body">
+            <form action="<?php echo site_url('Grupos/insertargrupo');?>" method="post" enctype="multipart/form-data" id = "datosgrupo"> 
+              <div class="espacioDos"></div>
+              <div class="row">
+                <div class="col">
                   Nombre <input type="text" name="nombre" id="nombre" class="form-control form-control-sm">
-                  </div>
-                  <div class="col">
-                    Estatus
-                    <select class="form-control form-control-sm" name="estatus" id="estatus" required="">
-                    <option value="">Seleccione una opción</option>
-                    <option value="1">Activo</option>
-                    <option value="0">Inactivo</option>
-                    </select>
-                  </div>
                 </div>
-
-                <div class="espacioDos"></div>
-
-
-                <div class="row">
-                  <div class="col">
+                <div class="col">
+                  Estatus
+                    <select class="form-control form-control-sm" name="estatus" id="estatus" required="">
+                      <option value="">Seleccione una opción</option>
+                      <option value="1">Activo</option>
+                      <option value="0">Inactivo</option>
+                    </select>
+                </div>
+              </div>
+              <div class="espacioDos"></div>
+              <div class="row">
+                <div class="col">
                   Unidad de negocio
                   <select class="form-control form-control-sm" name="unidad_negocio" id="unidad_negocio" required="">
-                  <option value="">Selecciona una opción</option>
-                  <?php foreach(getUnidadNegocio() as $fila){?>
+                    <option value="">Selecciona una opción</option>
+                    <?php foreach(getUnidadNegocio() as $fila){?>
                     <option value="<?php echo $fila->id?>"><?php echo $fila->nombre?></option>
                     <?php } ?> 
                   </select>
-                  </div>
-                  <div class="col">
+                </div>
+                <div class="col">
                   Plantel
                   <select class="form-control form-control-sm" name="plantel" id="plantel" required="">
-                  <option value="">Selecciona una opción</option> 
+                    <option value="">Selecciona una opción</option> 
                   </select>
-                  </div>
                 </div>
+              </div>
 
 
-                <div class="espacioDos"></div>
+              <div class="espacioDos"></div>
 
               <div class="row">
-              <div class="col">
+                <div class="col">
                   Curso
-                  <select class="form-control form-control-sm" name="curso" id="curso" required="">
-                  <option value="">Selecciona una opción</option>
-                  <?php foreach(CatalagoGetCursos() as $fila){?>
-                    <option value="<?php echo $fila->id?>"><?php echo $fila->nombre?></option>
-                    <?php } ?> 
-                  </select>
-                  </div>
-
-                  <div class="col">
-                  Horarios
-                    <select class="form-control form-control-sm" name="horarios" id="horarios" required="">
-                    <option value="">Seleccione una opción</option>
-                   <?php foreach(getAllHorarios() as $fila){?>
-                    <option value="<?php echo $fila->id; ?>"><?php echo $fila->nombre;?></option>
-                   <?php } ?>
+                    <select class="form-control form-control-sm" name="curso" id="curso" required="">
+                      <option value="">Selecciona una opción</option>
+                      <?php foreach(CatalagoGetCursos() as $fila){?>
+                      <option value="<?php echo $fila->id?>"><?php echo $fila->nombre?></option>
+                      <?php } ?> 
                     </select>
                   </div>
-                </div>
-
-                <div class="espacioDos"></div>
-
-                <div class="row">
                 <div class="col">
-                    Salon
-                    <select class="form-control form-control-sm" name="salon" id="salon" required="">
+                  Horarios
+                    <select class="form-control form-control-sm" name="horarios" id="horarios" required="">
+                      <option value="">Seleccione una opción</option>
+                      <?php foreach(getAllHorarios() as $fila){?>
+                      <option value="<?php echo $fila->id; ?>"><?php echo $fila->nombre;?></option>
+                      <?php } ?>
+                    </select>
+                </div>
+              </div>
+
+              <div class="espacioDos"></div>
+
+              <div class="row">
+                <div class="col">
+                  Salon
+                  <select class="form-control form-control-sm" name="salon" id="salon" required="">
                     <option value="">Seleccione una opción</option>
                     <?php foreach(getAllSalones() as $fila){ ?>
                     <option value="<?php echo $fila->id; ?>"><?php echo $fila->nombre;?></option>
                     <?php } ?>
-                    </select>
-                  </div>
-                  <div class="col">
+                  </select>
+                </div>
+                <div class="col">
                   Cupo
                     <input type="number" name="cupo" id="cupo" class="form-control form-control-sm">
-                  </div>
                 </div>
+              </div>
                 
-                <div class="espacioDos"></div>
-
-                <div class="row">
-                  <div class="col">
+              <div class="espacioDos"></div>
+              <div class="row">
+                <div class="col">
                   Nivel
-                    <select class="form-control form-control-sm" name="nivel" id="nivel" required="">
+                  <select class="form-control form-control-sm" name="nivel" id="nivel" required="">
                     <option value="">Seleccione una opción</option>
                     <?php foreach(getNivel() as $fila){ ?>
-                     <option value="<?php echo $fila->id; ?>"><?php echo $fila->nombre;?></option>
+                    <option value="<?php echo $fila->id; ?>"><?php echo $fila->nombre;?></option>
                     <?php } ?>
-                    </select>
-                  </div>
-                  <div class="col">
+                  </select>
+                </div>
+                <div class="col">
                   Ciclo
                     <select class="form-control form-control-sm" name="ciclo" id="ciclo" required="">
-                    <option value="">Seleccione una opción</option>
-                    <?php foreach(CatalagogetGetCiclo() as $fila){ ?>
-                     <option value="<?php echo $fila->id; ?>"><?php echo $fila->nombre;?></option>
-                    <?php } ?>
+                      <option value="">Seleccione una opción</option>
+                      <?php foreach(CatalagogetGetCiclo() as $fila){ ?>
+                      <option value="<?php echo $fila->id; ?>"><?php echo $fila->nombre;?></option>
+                      <?php } ?>
                     </select>
-                  </div>
-                  
-                  <div class="espacioDos"></div>
-
-                  <div class="col">
-                    Cargar Imagen
+                </div>
+              </div>
+              <div class="row">   
+                <div class="espacioDos"></div>
+                <div class="col">
+                  Cargar Imagen
                     <div class="form-group">
-                    <input  class="form-control form-control-sm" type="file" id="imagen_grupo" name="imagen_grupo" required="">
-                  </div>
-                  </div>
-                  <div class="col">
+                      <input  class="form-control form-control-sm" type="file" id="imagen_grupo" name="imagen_grupo" required="">
+                    </div>
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col">
                   Teacher 1
-                    <select class="form-control form-control-sm" name="maestro" id="maestro">
+                  <select class="form-control form-control-sm" name="maestro" id="maestro">
                     <option value="">Seleccione una opción</option>
                     <?php foreach(MaestrosGetAllMaestros() as $fila){ ?>
-                     <option value="<?php echo $fila->id_usuario; ?>"><?php echo $fila->nombre.' '.$fila->apellido_paterno.' '.$fila->apellido_materno;?></option>
+                    <option value="<?php echo $fila->id_usuario; ?>"><?php echo $fila->nombre.' '.$fila->apellido_paterno.' '.$fila->apellido_materno;?></option>
                     <?php } ?>
-                    </select>
-                  </div>
-                  <div class="col">
+                  </select>
+                </div>
+                <div class="col">
                   Teacher 2
                     <select class="form-control form-control-sm" name="maestro2" id="maestro2">
-                    <option value="">Seleccione una opción</option>
+                      <option value="">Seleccione una opción</option>
                     </select>
-                  </div>
-                  
                 </div>
+              </div>
                 
-                <div class="espacioDos"></div>
+              <div class="espacioDos"></div>
 
               <div class="form-group">
                 <label for="lblInstrucciones">Descripción</label>
@@ -212,115 +198,86 @@
               <button type="submit" name="submitGP" class="btn btn-primary btn-sm">Registrar</button>
 
               <button class="btn btn-secondary btn-sm" onclick="confirmarlimpiado()">Limpiar</button>
-              </form>
-
-              </div>
-              </div>
-          </div>
-
-
-
-
-        </div>  
-      </div>
-    </div>
-
-
-      <div class="espacioDos"></div>
-      <div class="espacioDos"></div>
-      <div class="espacioDos"></div>
-      
-      <div class="espacioDos"></div>
-      <div class="espacioDos"></div>
-      <div class="espacioDos"></div>
-
-      <div class="container">
-        <div class="row">
-          <div class="col-md-6">
-            <p class="mt-5 mb-3 text-muted text-center"> © <span class="brain-foot">Brain</span> <?php echo date("Y");?></p>
-          </div>
-          <div class="col-md-3">
-          <ul class="footer">
-            <li><a href="">Aviso de privacidad</a>
-            </li>
-            <li><a href="">Certificado AMIPCI</a>
-            </li>
-            <li><a href="">Certificado Pagos en Linea.</a>
-            </li>
-            <li><a href="">Certificado SSL.</a>
-            </li>
-          </ul>            
-          </div>
-                   
- 
-          <div class="col-md-3">
-          <ul class="footer">
-            <li><a href="">Company</a>
-            </li>
-            <li><a href="">About</a>
-            </li>
-            <li><a href="">Blog</a>
-            </li>
-            <li><a href="">Careers</a>
-            </li>
-            <li><a href="">Press</a>
-            </li>
-          </ul>         
+            </form>
           </div>
         </div>
       </div>
+    </div>  
+  </div>
+</div>
+
+<div class="espacioAmplio"></div>
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-6">
+      <p class="mt-5 mb-3 text-muted text-center"> © <span class="brain-foot">Brain</span> <?php echo date("Y");?></p>
+    </div>
+    <div class="col-md-3">
+      <ul class="footer">
+        <li><a href="">Aviso de privacidad</a></li>
+        <li><a href="">Certificado AMIPCI</a></li>
+        <li><a href="">Certificado Pagos en Linea.</a></li>
+        <li><a href="">Certificado SSL.</a></li>
+      </ul>            
+    </div>
+                   
+    <div class="col-md-3">
+      <ul class="footer">
+        <li><a href="">Company</a></li>
+        <li><a href="">About</a></li>
+        <li><a href="">Blog</a></li>
+        <li><a href="">Careers</a></li>
+        <li><a href="">Press</a></li>
+      </ul>         
+    </div>
+  </div>
+</div>
       
-      <div class="espacioDos"></div>
-      <div class="espacioDos"></div>
-      <div class="espacioDos"></div>
-      
-      <div class="espacioDos"></div>
-      <div class="espacioDos"></div>
-      <div class="espacioDos"></div>
-      <?php include(APPPATH.'Views/include/footer.php');?>
-      <?php include(APPPATH.'Views/include/header-js.php');?>
+<div class="espacioAmplio"></div>
+<?php include(APPPATH.'Views/include/footer.php');?>
+<?php include(APPPATH.'Views/include/header-js.php');?>
      
  
-    <script>
-      $('#unidad_negocio').change(function () {
-        var id_unidad = $(this).val();
-        // cuando el valor sea 0, se debe resetear el listado de planteles
-        $.ajax({
-          type: "POST",
-          url: "<?php echo site_url('Alumnos/plantelesUnidadNegocio');?>",
-          data: "id_unidad_negocio=" + id_unidad,
-          success : function(text){
-            document.getElementById("plantel").innerHTML = "";
-            $('#plantel').append(text);
-        }
-
-        });
-      });
-
-      $('#maestro').change(function () {
-        var id_maestro1 = $(this).val();
-        if(id_maestro1 != ""){
-          $.ajax({
-          type: "POST",
-          url: "<?php echo site_url('Grupos/ajaxlistadomaestrostipo2');?>",
-          data: {id_maestro1},
-          success : function(text){
-            document.getElementById("maestro2").innerHTML = "";
-            $('#maestro2').append(text);
-          }
-        });
-        }else{
-          document.getElementById("maestro2").innerHTML="";
-          $('#maestro2').append('<option value="">Seleccione una opción</option>\
-          </select>');
-        }
-      });
-
-
-      function confirmarlimpiado(){
-       if(confirm("Seguro que quieres limpiar el formulario")){
-        document.getElementById("datosgrupo").reset();
-       }
+<script>
+  $('#unidad_negocio').change(function () {
+    var id_unidad = $(this).val();
+      $.ajax({
+        type: "POST",
+        url: "<?php echo site_url('Alumnos/plantelesUnidadNegocio');?>",
+        data: "id_unidad_negocio=" + id_unidad,
+        success : function(text){
+        document.getElementById("plantel").innerHTML = "";
+        $('#plantel').append(text);
       }
-    </script>
+
+    });
+  });
+
+  $('#maestro').change(function () {
+    var id_maestro1 = $(this).val();
+    if(id_maestro1 != ""){
+      $.ajax({
+      type: "POST",
+      url: "<?php echo site_url('Grupos/ajaxlistadomaestrostipo2');?>",
+      data: {id_maestro1},
+      success : function(text){
+        document.getElementById("maestro2").innerHTML = "";
+          $('#maestro2').append(text);
+        }
+      });
+    }else{
+      document.getElementById("maestro2").innerHTML="";
+      $('#maestro2').append('<option value="">Seleccione una opción</option>\
+        </select>');
+    }
+  });
+
+
+function confirmarlimpiado(){
+  if(confirm("Seguro que quieres limpiar el formulario")){
+    document.getElementById("datosgrupo").reset();
+    }
+  }
+</script>
  
