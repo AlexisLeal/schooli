3eaf15bed20220480a45b4297c330787447f7f5f
AlexisLeal <31640145+AlexisLeal@users.noreply.github.com>
2021-01-13 13:20:52 -0600

    Calificaciones
    Se agrega la foma de las calificaciones

--- app/Controllers/Calificaciones.php
+++ app/Controllers/Calificaciones.php
@@ -1,4 +1,5 @@
 <?php namespace App\Controllers;
+use  App\Models\Karedex_model;
 helper('Calificar');
 
 class Calificaciones extends BaseController{
@@ -8,10 +9,17 @@ class Calificaciones extends BaseController{
         if($this->session->get('login') && $this->session->get('roll') == 4){
             if(isset($_POST['SubmitObtenerCalificaciones'])){
                 $REQUEST = \Config\Services::request();
+                $hoy = date("Y-m-d H:i:s");
                 $IdGrupo = $REQUEST->getPost('IdGrupo');
                 $IdCurso = $REQUEST->getPost('IdCurso');
                 $IdNivel = $REQUEST->getPost('IdNivel');
                 $IdCiclo = $REQUEST->getPost('IdCiclo');
+                $ValordeEjercicios = $REQUEST->getPost('ValordeEjercicios');
+                $ValordeExamanes = $REQUEST->getPost('ValordeExamanes');
+                $CantidadeEjericios = $REQUEST->getPost('CantidadeEjericios');
+                $CantidadeExamenes = $REQUEST->getPost('CantidadeExamenes');
+                $ValordeCadaEjercio = $ValordeEjercicios/$CantidadeEjericios;
+                $ValordeCadaExamen = $$ValordeExamanes/$CantidadeExamenes;
                 $CalificacionFinalEvaluaciones = 0;
                 foreach(CalificarObtenerMiembrosdeGrupo($IdGrupo) as $FilaMiembro){
                     foreach(CalificarGetEvaluacionesContestadas($FilaMiembro->id,$IdGrupo,$IdCurso,$IdNivel,$IdCiclo) as $FilaEvaluacion){
@@ -19,29 +27,34 @@ class Calificaciones extends BaseController{
 
                             if($FilaTipoyCategoria->tipo_evaluacion == 1){
                                 if(($FilaTipoyCategoria->idCategoriaEvaluacion) == 1){
-                                    $FilaEvaluacion->calificacion;
+                                    $CalificacionFinalEvaluaciones =$FilaEvaluacion->calificacion * $ValordeCadaEjercio;
                                     //Aplica la ponderacion 
 
                                 }elseif(($FilaTipoyCategoria->idCategoriaEvaluacion) == 2){
-                                    $FilaEvaluacion->calificacion;
+                                    $CalificacionFinalEvaluaciones =$FilaEvaluacion->calificacion * ($ValordeCadaEjercio * 0.50);
                                     //Aplica la ponderacion
                                 }
 
                             }elseif($FilaTipoyCategoria->tipo_evaluacion == 2){
                                 if(($FilaTipoyCategoria->idCategoriaEvaluacion) == 1){
-                                    $FilaEvaluacion->calificacion;
+                                    $CalificacionFinalEvaluaciones = $FilaEvaluacion->calificacion *  $ValordeCadaExamen;
                                     //Aplica la ponderacion 
 
                                 }elseif(($FilaTipoyCategoria->idCategoriaEvaluacion) == 2){
-                                    $FilaEvaluacion->calificacion;
+                                    $CalificacionFinalEvaluaciones =$FilaEvaluacion->calificacion *  ($ValordeCadaExamen * 0.50);
                                     //Aplica la ponderacion
                                 }
 
                             }
-
                     }
 
                     //Insertamos la calificacion y reiniciamos la calificaicon  a cero 
+                    $data = [ 'id_usuario' => $FilaMiembro->id,
+                    'id_nivel' => $IdNivel, 
+                    'primera_oportunidad' =>$$CalificacionFinalEvaluaciones,
+                    'fecha_creacion' => $hoy,
+                    'fecha_ultimo_cambio' => $hoy,
+                    ];
                     $CalificacionFinalEvaluaciones = 0;
                 }
 
---
+++ app/Models/Kardex_model.php
@@ -0,0 +1,28 @@
+<?php namespace App\Models;
+
+use CodeIgniter\Model;
+
+class Karedex_model extends Model
+{
+    
+    protected $table      = 'kardex';
+    
+    protected $primaryKey = 'id';
+
+    protected $returnType     = 'array';
+    protected $useSoftDeletes = true;
+
+    
+    protected $allowedFields = ['id_usuario','id_nivel','primera_oportunidad','segunda_oportunidad','fecha_creacion',
+    'fecha_ultimo_cambio'];
+
+    protected $useTimestamps = false;
+    protected $createdField  = 'fecha_creacion';
+    protected $updatedField  = 'fecha_ultimo_cambio';
+    protected $deletedField  = 'deleted';
+
+    #Reglas de validacion 
+    protected $validationRules    = [];
+    protected $validationMessages = [];
+    protected $skipValidation     = false;
+}
