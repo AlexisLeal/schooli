97aedc5a5c5e684aadd6fad414ab4f8d9e5ee7f2
iscgerardoflores <isc.gerardoflores@gmail.com>
2021-01-26 17:53:10 -0600

    gf grupo helper

--- app/Helpers/Grupos_helper.php
+++ app/Helpers/Grupos_helper.php
@@ -171,6 +171,22 @@ function GruposInsertaDatosTablaControlCursoCiclo($nombreTabla,$idGrupo,$idCurso
                 }
                 /** Termina
                  *  funcion GF */
+                // Recorrer arreglo de datos
+                    // Dentro de arreglo de datos, recorrer arreglo de frecuencia de dias que tiene 1
+                        //  Insertar el registro en la tabla
+                        foreach($datos as $registros){
+                            echo "Hola";
+                            //obtener cada campo
+                            $fechaCiclo      = $registros[0];//es la fecha
+                            $numSemana       = $registros[1];//es el numero de semana anual
+                            $nombreDiaSemana = $registros[2];//es el nombre del día de la semana
+
+                            $diasdeFrecuencia; // arreglo con los dias de la frecuencia, regresa Lu,Ma,Mi,Ju,Vi,Sa,Do
+
+                                 recorrer dias frecuanci y cuando el dia sea igual a $nombreDiaSemana, hacer el insert
+                        }
+
+
                 $diasdeFrecuanciaBD = $diasdeFrecuancia[$aux];
                 $query = "INSERT INTO $nombreTabla (idgrupo,idcurso,idciclo,idnivel,numerosemanaincremental,numerosemanaanual,sesion,fecha,dia) 
                 VALUES ($idGrupo,$idCurso,$idCiclo,$idNivel,'$semanaincremental',numerosemanaanual,$sesion,'FECHA',$diasdeFrecuanciaBD";
