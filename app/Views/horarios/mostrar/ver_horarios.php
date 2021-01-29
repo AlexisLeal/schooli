c8fba1908327cdc91b0930eb7bb89d442ca20600
iscgerardoflores <isc.gerardoflores@gmail.com>
2021-01-29 15:06:29 -0600

    gf inlcude antes - footer

--- app/Views/horarios/mostrar/ver_horarios.php
+++ app/Views/horarios/mostrar/ver_horarios.php
@@ -114,56 +114,11 @@
 </div>
 
 
-<div class="espacioDos"></div>
-<div class="espacioDos"></div>
-<div class="espacioDos"></div>
+<div class="espacioAmplio"></div>
 
-<div class="espacioDos"></div>
-<div class="espacioDos"></div>
-<div class="espacioDos"></div>
+<?php include("include/antes-footer.php");?>
 
-<div class="container">
-  <div class="row">
-    <div class="col-md-6">
-      <p class="mt-5 mb-3 text-muted text-center"> © <span class="brain-foot">LMS INBI</span> <?php echo date("Y"); ?></p>
-    </div>
-    <div class="col-md-3">
-      <ul class="footer">
-        <li><a href="">Aviso de privacidad</a>
-        </li>
-        <li><a href="">Certificado AMIPCI</a>
-        </li>
-        <li><a href="">Certificado Pagos en Linea.</a>
-        </li>
-        <li><a href="">Certificado SSL.</a>
-        </li>
-      </ul>
-    </div>
+<div class="espacioAmplio"></div>
 
-
-    <div class="col-md-3">
-      <ul class="footer">
-        <li><a href="">Company</a>
-        </li>
-        <li><a href="">About</a>
-        </li>
-        <li><a href="">Blog</a>
-        </li>
-        <li><a href="">Careers</a>
-        </li>
-        <li><a href="">Press</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</div>
-
-<div class="espacioDos"></div>
-<div class="espacioDos"></div>
-<div class="espacioDos"></div>
-
-<div class="espacioDos"></div>
-<div class="espacioDos"></div>
-<div class="espacioDos"></div>
 <?php include(APPPATH . 'Views/include/footer.php'); ?>
 <?php include(APPPATH . 'Views/include/header-js.php'); ?>
